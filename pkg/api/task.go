2cd47ead4ec4e414f27dfe0b3674b1f63ab2fedd
Elena Mask <Elenamask007@yandex.ru>
2025-07-31 18:13:19 +0300

    sixt step done

--- pkg/api/task.go
+++ pkg/api/task.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/ElenaMask/go_final_project/pkg/db"
@@ -30,6 +31,10 @@ func TaskHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost:
 		addTaskHandler(w, r)
+	case http.MethodGet:
+		getTaskHandler(w, r)
+	case http.MethodPut:
+		updateTaskHandler(w, r)
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
@@ -60,6 +65,74 @@ func addTaskHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	writeJSON(w, Response{ID: fmt.Sprintf("%d", id)})
+}
+
+func getTaskHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		writeError(w, "Не указан идентификатор")
+		return
+	}
+
+	t, err := db.GetTask(id)
+	if err != nil {
+		writeError(w, "Задача не найдена")
+		return
+	}
+	apiTask := &APITask{
+		ID:      strconv.FormatInt(t.ID, 10),
+		Date:    t.Date,
+		Title:   t.Title,
+		Comment: t.Comment,
+		Repeat:  t.Repeat,
+	}
+
+	writeJSON(w, apiTask)
+}
+
+func updateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var apiTask APITask
+
+	if err := json.NewDecoder(r.Body).Decode(&apiTask); err != nil {
+		writeError(w, "Некорректный формат JSON")
+		return
+	}
+	id, err := strconv.ParseInt(apiTask.ID, 10, 64)
+	if err != nil {
+		writeError(w, "Некорректный идентификатор задачи")
+		return
+	}
+
+	task := db.Task{
+		ID:      id,
+		Date:    apiTask.Date,
+		Title:   apiTask.Title,
+		Comment: apiTask.Comment,
+		Repeat:  apiTask.Repeat,
+	}
+
+	if task.ID == 0 {
+		writeError(w, "Не указан идентификатор задачи")
+		return
+	}
+
+	if task.Title == "" {
+		writeError(w, "Не указан заголовок задачи")
+		return
+	}
+
+	if err := checkDate(&task); err != nil {
+		writeError(w, err.Error())
+		return
+	}
+
+	err = db.UpdateTask(&task)
+	if err != nil {
+		writeError(w, "Задача не найдена")
+		return
+	}
+
+	writeJSON(w, Response{})
 }
 
 func checkDate(task *db.Task) error {
